0a691c11f15776680f2dbf6efcbf583854379c92
Teemu Reuna <teemu.reuna@unikie.com>
2020-02-10 11:14:52 +0200

    added reducers

--- frontend/src/_alerts/user.actions.js
+++ frontend/src/_alerts/user.actions.js
@@ -1,4 +1,5 @@
 import * as userConstants from '../_constants/user.constants'
+import * as userServices from '../_services/user.services'
 import * as alertActions from './alert.actions'
 import history from '../_helpers/history'
 
@@ -10,7 +11,7 @@ export function login(username, password) {
   return (dispatch) => {
     dispatch(request({ username }))
 
-    login(username, password)
+    userServices.login(username, password)
       .then(
         (user) => {
           dispatch(success(user))
@@ -25,6 +26,62 @@ export function login(username, password) {
 }
 
 export function logout() {
-  logout()
+  userServices.logout()
   return { type: userConstants.LOGOUT}
+}
+
+export function register(user) {
+
+  function request(user) { return { type: userConstants.REGISTER_REQUEST, user } }
+  function success(user) { return { type: userConstants.REGISTER_SUCCESS, user } }
+  function failure(error) { return { type: userConstants.REGISTER_FAILURE, error } }
+
+  return (dispatch) => {
+    dispatch(request(user))
+
+    userServices.request(user)
+      .then(
+        () => {
+          dispatch(success())
+          history.push('/login')
+          dispatch(alertActions.success('Registration successful'))
+        },
+        (error) => {
+          dispatch(failure(error.toString()))
+          dispatch(alertActions.error(error.toString()))
+        }
+      )
+  }
+}
+
+export function getAll() {
+  function request() { return { type: userConstants.GETALL_REQUEST } }
+  function success(users) { return { type: userConstants.GETALL_SUCCESS, users } }
+  function failure(error) { return { type: userConstants.GETALL_FAILURE, error } }
+
+  return (dispatch) => {
+    dispatch(request())
+
+    userServices.getAll()
+      .then(
+        (users) => dispatch(success(users)),
+        (error) => dispatch(failure(error.toString()))
+        )
+  }
+}
+
+export function DeleteUser(id) {
+  function request(id) { return { type: userConstants.DELETE_REQUEST, id } }
+  function success(id) { return { type: userConstants.DELETE_SUCCESS, id } }
+  function failure(id, error) { return { type: userConstants.DELETE_FAILURE, id, error } }
+
+  return (dispatch) => {
+    dispatch(request(id))
+
+    userServices.deleteUser(id)
+      .then(
+        (user) => dispatch(success(id)),
+        (error) => dispatch(failure(id, error.toString()))
+        )
+  }
 }
---
+++ frontend/src/_reducers/alert.reducer.js
@@ -0,0 +1,20 @@
+import * as alertConstants from '../_constants/alert.constants'
+
+export default function alert(state = {}, action) {
+  switch (action.type) {
+    case alertConstants.SUCESS:
+      return {
+        type: 'alert-success',
+        message: action.message
+      }
+    case alertConstants.ERROR:
+      return {
+        type: 'alert-danger',
+        message: action.message
+      }
+    case alertConstants.CLEAR:
+      return {}
+    default:
+      return state
+  }
+}
